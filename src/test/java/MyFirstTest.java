9f3e634c27ea1b1bf2e1b6e2ba39c670e23b4dc0
ilyapesin <pesinilya@gmail.com>
2024-02-26 20:16:04 +0200

    webinar4

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void test_start() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://demoqa.com/");
+        Thread.sleep(10000);
+    }
+}
